4d54ab84aa91f8315acbd4cb5cd0050a6a8d2f3a
phxpy <phxpy@protonmail.com>
2022-05-05 03:56:23 +0300

    added home page template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route("/")
-def index():
-    return "<h1>Hey!</h1>"
+@app.route("/")
+def home():
+    return render_template("home.html")
